21505628be9634558fdca44ad04e8b0ef65bacc7
Shreyansh <shreyanshrana@gmail.com>
2021-04-20 17:48:38 +0530

    Added draggable windows

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 
 import './App.css';
 import { ApplicationBar } from './components/ApplicationBar/ApplicationBar';
-import { Browser } from './components/Browser/Browser';
 import { Panel } from './components/Panel/Panel';
-import { useDrop } from 'react-dnd'
-import { ItemTypes } from './DraggableItems';
 import { WindowSpace } from './components/WindowSpace/WindowSpace';
 
 function App() {
--- src/components/Browser/Browser.js
+++ src/components/Browser/Browser.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import { useDrag } from 'react-dnd'
+import React, { useState } from 'react'
 import { TopBar } from '../TopBar/TopBar'
 
 export const Browser = (props) => {
-    // const [{ isDragging }, drag] = useDrag(
-    //     () => ({
-    //       type: "browser",
-    //       collect: (monitor) => ({
-    //         isDragging: monitor.isDragging()
-    //       })
-    //     }),
-    //     []
-    //   )
-    
+    const [url, setURL] = useState("https://www.google.com/webhp?igu=1");
     return (
         <div 
-            style={{ width:props.width, borderTopLeftRadius:'10px',  borderTopRightRadius:'10px'}} >
+            style={{ width:props.width, borderTopLeftRadius:'10px',  borderTopRightRadius:'10px', height:""}} >
             <TopBar title={props.title}/>
-            hello
+            <div className="w-full bg-gray-500">
+                <div className="w-1/12 inline-block text-center text-xl">
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                    <ion-icon name="refresh-outline"></ion-icon>
+                </div>
+                <input className="w-10/12 outline-none border-none" style={{height:"24px"}}/>
+            </div>
+            <iframe src={url} title="W3Schools Free Online Web Tutorials" width="100%" frameBorder="0" height="340px">
+            </iframe>
         </div>
     )
 }
