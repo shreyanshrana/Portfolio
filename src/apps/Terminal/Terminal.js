382577d27e01a05a436f82c25fb733ef83142b04
Shreyansh <shreyanshrana@gmail.com>
2021-05-04 16:30:51 +0530

    Added terminal.js

--- src/apps/Terminal/Terminal.js
+++ src/apps/Terminal/Terminal.js
@@ -21,91 +21,90 @@ export const Terminal = () => {
         }
         if(e.key === "Enter")
         {
-            
-        let label = document.createElement("label");
-        let input = document.createElement("input");
-        input.classList.add("bg-black");
-        input.classList.add("mx-1");
-        input.classList.add("text-white");
-        input.classList.add("p-1");
-        input.classList.add("focus:outline-none");
-        input.classList.add("terminalOutput");
-        input.value = cmd;
-        input.disabled = true;        
-        label.innerHTML = "shreyansh@potato-pc " + displayCurrDir + " > ";
-        label.classList.add("terminalOutput");
-        document.getElementById("terminalLabel").before(label)
-        document.getElementById("terminalLabel").before(input);
-        document.getElementById("terminalLabel").before(a);
-        document.getElementById("terminalInput").value = "";
-        // document.getElementById("terminalWidget").(a)
-        let i = 0;
-        for(let a  in cmd){
-            i++;
-            if(cmd[a] === " ")
-                break;
+            let label = document.createElement("label");
+            let input = document.createElement("input");
+            input.classList.add("bg-black");
+            input.classList.add("mx-1");
+            input.classList.add("text-white");
+            input.classList.add("p-1");
+            input.classList.add("focus:outline-none");
+            input.classList.add("terminalOutput");
+            input.value = cmd;
+            input.disabled = true;        
+            label.innerHTML = "shreyansh@potato-pc " + displayCurrDir + " > ";
+            label.classList.add("terminalOutput");
+            document.getElementById("terminalLabel").before(label)
+            document.getElementById("terminalLabel").before(input);
+            document.getElementById("terminalLabel").before(a);
+            document.getElementById("terminalInput").value = "";
+            // document.getElementById("terminalWidget").(a)
+            let i = 0;
+            for(let a  in cmd){
+                i++;
+                if(cmd[a] === " ")
+                    break;
+            }
+            let command = "";
+            let param =  "";
+            if(i !== cmd.length){
+                command = cmd.substr(0,i-1);
+                param = cmd.substr(i,cmd.length-1);
+            }
+            else{
+                command = cmd.substr(0,i);
+            }
+            // console.log(command);
+            // console.log(param);
+            // console.log(currDir);
+            // console.log(DirectoryPath);
+            switch(command){
+                case 'sudo':
+                    a.classList.add("text-blue-500");
+                    a.innerHTML = "(¬‿¬) someone wants admin privileges... <br/> sadly IMMA SAY NO ~(˘▾˘~)";
+                    break;
+                case 'su':
+                    a.classList.add("text-blue-500");
+                    a.innerHTML = "(¬‿¬) someone wants admin privileges... <br/> sadly IMMA SAY NO ~(˘▾˘~)";
+                    break;
+                case 'clear':
+                    Array.from(document.getElementsByClassName("terminalOutput")).map(e => {
+                        e.remove();
+                        return null;
+                    });
+                    Array.from(document.getElementsByClassName("terminalInput")).map(e => {
+                        e.remove();
+                        return null;
+                    });
+                    break;
+                case 'ls':
+                    a.classList.add("text-yellow-500");
+                    for(let k in currDir){
+                        a.innerHTML += k + " "; 
+                    }
+                    // a.innerHTML = ""
+                    break;
+                case 'echo':
+                    a.classList.add("text-white");
+                    a.innerHTML = param;
+                    break;
+                case 'cd':
+                    if(currDir[param]){
+                        setCurrDir(currDir[param]);
+                        setDisplayCurrDir("~/"+ param);
+                    }
+                    else if(param === "../" && JSON.stringify(currDir) !== JSON.stringify(DirectoryPath)){
+                        setCurrDir(DirectoryPath);
+                        setDisplayCurrDir("~/");
+                    }
+                    else{
+                        a.classList.add("text-red-500");     
+                        a.innerHTML = "Directory doesnt exist";
+                    }
+                    break;
+                default:
+                    a.innerHTML += "Invalid command, please try again :)"
+            }
         }
-        let command = "";
-        let param =  "";
-        if(i !== cmd.length){
-            command = cmd.substr(0,i-1);
-            param = cmd.substr(i,cmd.length-1);
-        }
-        else{
-            command = cmd.substr(0,i);
-        }
-        // console.log(command);
-        // console.log(param);
-        // console.log(currDir);
-        // console.log(DirectoryPath);
-        switch(command){
-            case 'sudo':
-                a.classList.add("text-blue-500");
-                a.innerHTML = "(¬‿¬) someone wants admin privileges... <br/> sadly IMMA SAY NO ~(˘▾˘~)";
-                break;
-            case 'su':
-                a.classList.add("text-blue-500");
-                a.innerHTML = "(¬‿¬) someone wants admin privileges... <br/> sadly IMMA SAY NO ~(˘▾˘~)";
-                break;
-            case 'clear':
-                Array.from(document.getElementsByClassName("terminalOutput")).map(e => {
-                    e.remove();
-                    return null;
-                });
-                Array.from(document.getElementsByClassName("terminalInput")).map(e => {
-                    e.remove();
-                    return null;
-                });
-                break;
-            case 'ls':
-                a.classList.add("text-yellow-500");
-                for(let k in currDir){
-                    a.innerHTML += k + " "; 
-                }
-                // a.innerHTML = ""
-                break;
-            case 'echo':
-                a.classList.add("text-white");
-                a.innerHTML = param;
-                break;
-            case 'cd':
-                if(currDir[param]){
-                    setCurrDir(currDir[param]);
-                    setDisplayCurrDir("~/"+ param);
-                }
-                else if(param === "../" && JSON.stringify(currDir) !== JSON.stringify(DirectoryPath)){
-                    setCurrDir(DirectoryPath);
-                    setDisplayCurrDir("~/");
-                }
-                else{
-                    a.classList.add("text-red-500");     
-                    a.innerHTML = "Directory doesnt exist";
-                }
-                break;
-            default:
-                a.innerHTML += "Invalid command, please try again :)"
-        }
-    }
     document.getElementById("terminalInput").focus();
         // document.getElementById("terminalInput").scrollTo = document.getElementById("terminalInput").scrollHeight;
 }
